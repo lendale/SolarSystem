ffd404003aaedff87e714273c394175de2697df5
lendale <nasibag1>
2016-01-07 18:01:20 +0800

    First commit. Spinner doesn't work.

--- app/src/androidTest/java/sidespell/tech/solarsystem/ApplicationTest.java
+++ app/src/androidTest/java/sidespell/tech/solarsystem/ApplicationTest.java
@@ -3,22 +3,22 @@ package sidespell.tech.solarsystem;
 import android.app.Activity;
 import android.app.Application;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.test.ApplicationTestCase;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends Activity implements AdapterView.OnItemSelectedListener {
+public class ApplicationTest extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Spinner spinner = (Spinner) findViewById(R.id.planetSpinner);
+        ImageView planetImage = (ImageView) findViewById(R.id.planetImageView);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -30,17 +30,16 @@ public class ApplicationTest extends Activity implements AdapterView.OnItemSelec
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
-        spinner.setOnItemSelectedListener(this);
-    }
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+            }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
+            }
+        });
     }
 }
