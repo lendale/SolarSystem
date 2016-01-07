1e747af4a1b4b43cebdb39dc07601fa031e7a4c7
lendale <nasibag1>
2016-01-07 17:41:29 +0800

    Spinner won't display

--- app/src/androidTest/java/sidespell/tech/solarsystem/ApplicationTest.java
+++ app/src/androidTest/java/sidespell/tech/solarsystem/ApplicationTest.java
@@ -1,13 +1,46 @@
 package sidespell.tech.solarsystem;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 import android.test.ApplicationTestCase;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest extends Activity implements AdapterView.OnItemSelectedListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.planetSpinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
